ce0424bfad004ebc6373b7269e44d69eb846e63f
Jonas Collberg <jonas.collberg@gmail.com>
2021-08-26 13:29:59 +0200

    Modify `x-init-kb.sh`

--- roles/x/files/x-init-kb.sh
+++ roles/x/files/x-init-kb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Use this as display-setup-script in `lightdm.conf`.
 setxkbmap -model pc105 -layout us,us -variant colemak,altgr-intl -option -option "grp:shifts_toggle,ctrl:nocaps,terminate:ctrl_alt_bksp"
+xset r rate 300 30
